5377f7cbc74fb5f39e16dc1f641e753c00e47c94
takipciberlin-sketch <takipciberlin@gmail.com>
2026-05-02 11:03:15 +0200

    j

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,14 +1,12 @@
+// FirebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSy...", // Buraya kendi key'ini ileride koyabilirsin
-  authDomain: "nextlogi-live.firebaseapp.com",
-  databaseURL: "https://nextlogi-live-default-rtdb.firebaseio.com",
-  projectId: "nextlogi-live",
-  storageBucket: "nextlogi-live.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  // Burayı Firebase Console -> Proje Ayarları -> Web Uygulaması kısmından almalısın
+  apiKey: "SENIN_API_KEYIN",
+  databaseURL: "https://PROJE_ID-default-rtdb.firebaseio.com",
+  projectId: "PROJE_ID",
 };
 
 const app = initializeApp(firebaseConfig);
