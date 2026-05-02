417209e51f87b03d6ee256936b4c39203f921b5a
takipciberlin-sketch <takipciberlin@gmail.com>
2026-05-02 11:29:18 +0200

    ju

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,10 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// Firebase'den aldığın senin projene özel kimlik bilgileri
 const firebaseConfig = {
-    apiKey: "AIzaSyAXZkJp1HA4NSbaWJZzjkWy2UTFCMKEn1I",
-    authDomain: "nextlogi-v19.firebaseapp.com",
-    projectId: "nextlogi-v19",
-    storageBucket: "nextlogi-v19.firebasestorage.app",
-    messagingSenderId: "69939460458",
-    appId: "1:69939460458:web:7094f54f2789a608c94d13",
-    measurementId: "G-F6E0D0QWE7", // Buraya virgül koymayı unutma!
-    databaseURL: "https://nextlogi-v19-default-rtdb.europe-west1.firebasedatabase.app" // Yeni satırımız bu
-  };
+  apiKey: "AIzaSyAXZkJp1HA4NSbaWJZzjkWy2UTFCMKEn1I",
+  authDomain: "nextlogi-v19.firebaseapp.com",
+  projectId: "nextlogi-v19",
+  storageBucket: "nextlogi-v19.firebasestorage.app",
+  messagingSenderId: "69939460458",
+  appId: "1:69939460458:web:7094f54f2789a608c94d13",
+  measurementId: "G-F6E0D0QWE7",
+  databaseURL: "https://nextlogi-v19-default-rtdb.europe-west1.firebasedatabase.app"
+};
+
+// Firebase uygulamasını başlatıyoruz
+const app = initializeApp(firebaseConfig);
+
+// Veritabanı referansını oluşturup diğer dosyaların kullanımına açıyoruz
+export const db = getDatabase(app);
