c135011a82bda3d2d54ec320bf5770b15cf8fd8f
takipciberlin-sketch <takipciberlin@gmail.com>
2026-05-02 11:27:51 +0200

    s

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,4 +1,3 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAXZkJp1HA4NSbaWJZzjkWy2UTFCMKEn1I",
     authDomain: "nextlogi-v19.firebaseapp.com",
@@ -6,5 +5,6 @@ const firebaseConfig = {
     storageBucket: "nextlogi-v19.firebasestorage.app",
     messagingSenderId: "69939460458",
     appId: "1:69939460458:web:7094f54f2789a608c94d13",
-    measurementId: "G-F6E0D0QWE7"
+    measurementId: "G-F6E0D0QWE7", // Buraya virgül koymayı unutma!
+    databaseURL: "https://nextlogi-v19-default-rtdb.europe-west1.firebasedatabase.app" // Yeni satırımız bu
   };
